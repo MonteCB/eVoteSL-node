f578061e3c70dc6afbf29e09f3f34238f707ddbd
MonteCB <buultjens.15@cse.mrt.ac.lk>
2018-05-16 14:34:35 +0530

    deploy_2

--- app.js
+++ app.js
@@ -52,6 +52,14 @@ app.use('/users', users);
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
+
+app.get("/dashboard", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+app.get("/dashboard/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
 //start server
 app.listen(port, () => {
     console.log('Server started on port ' + port);
